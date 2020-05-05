80a92401ab10d4b9d50ca609a351c0a5a8747dfd
SandovalJr <edubizzle7@gmail.com>
2020-05-05 10:59:31 -0500

    new arrivals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { CarrouselComponent } from './components/home/carrousel/carrousel.component';
+import { NewArrivalsComponent } from './components/home/new-arrivals/new-arrivals.component';
 
 // iconos
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, CarrouselComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, CarrouselComponent, NewArrivalsComponent],
   // todo lo que tenga la palabra modulos 
   imports: [BrowserModule],
   providers: [],
