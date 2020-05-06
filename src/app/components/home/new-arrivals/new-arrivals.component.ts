aa685a5ca4dab3275bd1c3190659fe7da003c284
SandovalJr <edubizzle7@gmail.com>
2020-05-06 10:37:21 -0500

    se agregaron new arrivals

--- src/app/components/home/new-arrivals/new-arrivals.component.ts
+++ src/app/components/home/new-arrivals/new-arrivals.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { fromEvent, from } from 'rxjs';
+import { ProductsInterface } from 'src/app/interfaces/eCommerce.interfaces';
+import { Products } from './../../../data/data';
+import { take, tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-arrivals',
   templateUrl: './new-arrivals.component.html',
-  styleUrls: ['./new-arrivals.component.css']
+  styleUrls: ['./new-arrivals.component.css'],
 })
 export class NewArrivalsComponent implements OnInit {
+  public mostrar: boolean = false;
+  // vamos a traer el array de productos de la data
+  public ArrivalProducts: Array<ProductsInterface> = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.obtenerNewArrival();
   }
 
+  ngOnInit(): void {}
+
+  public MostrarDiv() {
+    this.mostrar = true;
+    // this.mostrar = !this.mostrar
+  }
+  public QuitarDiv() {
+    this.mostrar = false;
+  }
+
+  obtenerNewArrival() {
+    from(Products)
+      .pipe(
+        map((producto) => {
+          let DireccionImg = `../../../../assets/DATA/${producto.img}`;
+          return {
+            ...producto,
+            img: DireccionImg,
+          };
+        }),
+        take(3),
+        tap((value) => this.ArrivalProducts.push(value))
+      )
+      .subscribe((value) => console.log(value));
+  }
 }
