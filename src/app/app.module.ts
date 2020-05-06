51613e6bdb4a38fe616f12c7b0a79daf3c681fae
SandovalJr <edubizzle7@gmail.com>
2020-05-06 16:31:37 -0500

    Contact us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { CarrouselComponent } from './components/home/carrousel/carrousel.component';
 import { NewArrivalsComponent } from './components/home/new-arrivals/new-arrivals.component';
+import { GetOnListComponent } from './components/home/get-on-list/get-on-list.component';
 
 // iconos
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, CarrouselComponent, NewArrivalsComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, CarrouselComponent, NewArrivalsComponent, GetOnListComponent],
   // todo lo que tenga la palabra modulos 
   imports: [BrowserModule],
   providers: [],
---
+++ src/app/components/home/get-on-list/get-on-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-get-on-list',
+  templateUrl: './get-on-list.component.html',
+  styleUrls: ['./get-on-list.component.css']
+})
+export class GetOnListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
