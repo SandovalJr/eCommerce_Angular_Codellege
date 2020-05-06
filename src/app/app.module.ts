ee225e1aa7f35daf1b2ce646fd846e8d5a6fa0bb
SandovalJr <edubizzle7@gmail.com>
2020-05-06 17:28:56 -0500

    homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { HomeComponent } from './components/home/home.component';
 import { CarrouselComponent } from './components/home/carrousel/carrousel.component';
 import { NewArrivalsComponent } from './components/home/new-arrivals/new-arrivals.component';
 import { GetOnListComponent } from './components/home/get-on-list/get-on-list.component';
+import { BannerPromocionProductosComponent } from './components/home/banner-promocion-productos/banner-promocion-productos.component';
 
 // iconos
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, CarrouselComponent, NewArrivalsComponent, GetOnListComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, CarrouselComponent, NewArrivalsComponent, GetOnListComponent, BannerPromocionProductosComponent],
   // todo lo que tenga la palabra modulos 
   imports: [BrowserModule],
   providers: [],
---
+++ src/app/components/home/banner-promocion-productos/banner-promocion-productos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-promocion-productos',
+  templateUrl: './banner-promocion-productos.component.html',
+  styleUrls: ['./banner-promocion-productos.component.css']
+})
+export class BannerPromocionProductosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
