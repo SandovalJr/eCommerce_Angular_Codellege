68e24767f849b843dd0d1b019629ecb13a638655
SandovalJr <edubizzle7@gmail.com>
2020-05-07 09:55:28 -0500

    Se agregaron fotos dinamicas

--- src/app/components/home/banner-promocion-productos/banner-promocion-productos.component.ts
+++ src/app/components/home/banner-promocion-productos/banner-promocion-productos.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Products } from 'src/app/data/data';
+import { take, mapTo, pluck, map, tap, filter } from 'rxjs/operators';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-banner-promocion-productos',
   templateUrl: './banner-promocion-productos.component.html',
-  styleUrls: ['./banner-promocion-productos.component.css']
+  styleUrls: ['./banner-promocion-productos.component.css'],
 })
 export class BannerPromocionProductosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public FotosArray: Array<string> = [];
+  constructor() {
+    this.GetImage();
   }
 
+  ngOnInit(): void {}
+
+  public GetImage() {
+    for (let index = 0; index < 2; index++) {
+      const element = Math.floor(Math.random() * (Products.length - 1) + 1);
+      const temporal = Products[element];
+      console.log(temporal.img);
+      
+      this.FotosArray.push(`../../../../assets/DATA/${temporal.img}`);
+    }
+  }
 }
