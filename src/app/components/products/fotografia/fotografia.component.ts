7dc5a65f77dafb4b9bfe7f9423d97f62cd9fe0af
SandovalJr <edubizzle7@gmail.com>
2020-05-08 11:09:13 -0500

    SEMI FINALIZADO

--- src/app/components/products/fotografia/fotografia.component.ts
+++ src/app/components/products/fotografia/fotografia.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsInterface } from 'src/app/interfaces/eCommerce.interfaces';
+import { Products, Departments } from 'src/app/data/data';
+import { from } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-fotografia',
   templateUrl: './fotografia.component.html',
-  styleUrls: ['./fotografia.component.css']
+  styleUrls: ['./fotografia.component.css'],
 })
 export class FotografiaComponent implements OnInit {
+  public productsShowCamaras: Array<ProductsInterface> = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.getCamaras();
   }
 
+  ngOnInit(): void {}
+
+  public getCamaras() {
+    let departamento = Departments.find(
+      (departamento) => departamento.department == 'Fotografía'
+    );
+    from(Products)
+      .pipe(
+        filter((producto) => producto.department == departamento.id),
+
+        map((producto) => {
+          let urlImage = `../../../../assets/DATA/${producto.img}`;
+          console.log(urlImage);
+          return {
+            ...producto,
+            img: urlImage,
+          };
+        }),
+        tap((ProdutoActualizado) =>
+          this.productsShowCamaras.push(ProdutoActualizado)
+        )
+      )
+      .subscribe();
+  }
 }
