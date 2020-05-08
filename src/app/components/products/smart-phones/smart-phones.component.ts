853c6d7d641b1450b0e93bca6f8e965123155511
SandovalJr <edubizzle7@gmail.com>
2020-05-08 10:31:01 -0500

    LISTADO DE CELULARES FINALIZADO

--- src/app/components/products/smart-phones/smart-phones.component.ts
+++ src/app/components/products/smart-phones/smart-phones.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsInterface } from 'src/app/interfaces/eCommerce.interfaces';
+import { Products, Departments } from 'src/app/data/data';
+import { from } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-smart-phones',
   templateUrl: './smart-phones.component.html',
-  styleUrls: ['./smart-phones.component.css']
+  styleUrls: ['./smart-phones.component.css'],
 })
 export class SmartPhonesComponent implements OnInit {
+  public productsShow: Array<ProductsInterface> = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.GetProducts();
   }
 
+  ngOnInit(): void {}
+
+  public GetProducts() {
+    let Departamente = Departments.find(
+      (value) => (value.department = 'SmartPhones')
+    );
+
+    from(Products)
+      .pipe(
+        filter((producto) => producto.department == Departamente.id),
+
+        map((producto) => {
+          let urlImage = `../../../../assets/DATA/${producto.img}`;
+          console.log(urlImage);
+          return {
+            ...producto,
+            img: urlImage,
+          };
+        }),
+        tap((ProdutoActualizado) => this.productsShow.push(ProdutoActualizado))
+      )
+      .subscribe();
+  }
 }
