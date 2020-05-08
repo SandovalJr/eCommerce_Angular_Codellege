407a6a46c7eb1aa724cc68fab9d614035ee012ca
SandovalJr <edubizzle7@gmail.com>
2020-05-08 11:09:47 -0500

    Se agrego linea blanca , celulares y camaras

--- src/app/components/products/linea-blanca/linea-blanca.component.ts
+++ src/app/components/products/linea-blanca/linea-blanca.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsInterface } from 'src/app/interfaces/eCommerce.interfaces';
+import { Products, Departments } from 'src/app/data/data';
+import { from } from 'rxjs';
+import { filter, map, tap, find } from 'rxjs/operators';
 
 @Component({
   selector: 'app-linea-blanca',
   templateUrl: './linea-blanca.component.html',
-  styleUrls: ['./linea-blanca.component.css']
+  styleUrls: ['./linea-blanca.component.css'],
 })
 export class LineaBlancaComponent implements OnInit {
+  public productsShowLN: Array<ProductsInterface> = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.GetLB();
   }
 
+  ngOnInit(): void {}
+
+  public GetLB() {
+    let departamento = Departments.find(
+      (depa) => depa.department == 'Línea Blanca'
+    );
+
+    from(Products)
+      .pipe(
+        filter((producto) => producto.department === departamento.id),
+        map((producto) => {
+          let UrlImg = `../../../../assets/DATA/${producto.img}`;
+          console.log(UrlImg);
+          return {
+            ...producto,
+            img: UrlImg,
+          };
+        }),
+        tap((ProdutoActualizado) =>
+          this.productsShowLN.push(ProdutoActualizado)
+        )
+      )
+      .subscribe();
+  }
 }
