9c2188edb113fd1eb56cdff5ae3a231330d5bcaa
SandovalJr <edubizzle7@gmail.com>
2020-05-08 19:07:04 -0500

    finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,12 @@ import { FotografiaComponent } from './components/products/fotografia/fotografia
 import { LineaBlancaComponent } from './components/products/linea-blanca/linea-blanca.component';
 import { AllProductsComponent } from './components/products/all-products/all-products.component';
 import { VistUsComponent } from './components/home/vist-us/vist-us.component';
+import { CostumerCareComponent } from './components/home/costumer-care/costumer-care.component';
 
 // iconos
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeComponent, CarrouselComponent, NewArrivalsComponent, GetOnListComponent, BannerPromocionProductosComponent, FooterComponent, BlackFooterComponent, SmartPhonesComponent, FotografiaComponent, LineaBlancaComponent, AllProductsComponent, VistUsComponent],
+  declarations: [AppComponent, NavbarComponent, HomeComponent, CarrouselComponent, NewArrivalsComponent, GetOnListComponent, BannerPromocionProductosComponent, FooterComponent, BlackFooterComponent, SmartPhonesComponent, FotografiaComponent, LineaBlancaComponent, AllProductsComponent, VistUsComponent, CostumerCareComponent],
   // todo lo que tenga la palabra modulos 
   imports: [BrowserModule],
   providers: [],
---
+++ src/app/components/home/costumer-care/costumer-care.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-costumer-care',
+  templateUrl: './costumer-care.component.html',
+  styleUrls: ['./costumer-care.component.css']
+})
+export class CostumerCareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
